94753fe83022e645f0377f6eef556e27ccec591e
sniclasj <sniclasj@gmail.com>
2022-01-26 09:02:30 +0000

    Update script.js content

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click",function() {
             if (this.getAttribute("data-type")==="submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -38,7 +38,16 @@ function checkAnswer() {
 }
 
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText);
 
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert (`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`
+    }
 }
 
 function incrementScore() {
